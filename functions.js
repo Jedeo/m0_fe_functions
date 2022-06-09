df057cec4661ffa157d378f1c289378336ec7650
Eric Weissman <eric@example.com>
2022-06-09 16:08:28 -0600

    Add functions

--- functions.js
+++ functions.js
@@ -1,22 +1,37 @@
 // 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
+function printGreeting() {
+  console.log("Hello!");
 
+}
+console.log(printGreeting());
 
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
-
-
-// 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
-
+function getLocation(location){
+  console.log(location);
+}
+console.log(getLocation("Colorado"));
+// 3: Write a function that has 3 parameters: a string and two numbers.
+//The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job.
+//The function should print out a sentence that includes the name of
+//the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
+function jobListing(company, minSalary, maxSalary){
+  console.log(`${company} pay range is ${minSalary} to ${maxSalary}.`);
+}
+console.log(jobListing("jedeo",11000,10000));
 
 // 4: Write a function that satifies the following interaction pattern:
+function checkStock(numRemingIteam, item ){
+  console.log(`Your iteam is ${item} and there is ${numRemingIteam} it left.`);
 
-checkStock(4, "Coffee");
+}
+console.log(checkStock(4, "Coffee"));
 // => "Coffee is stocked"
 
-checkStock(3, "Tortillas");
+console.log(checkStock(3, "Tortillas"));
 // => "Tortillas - running LOW"
 
-checkStock(0, "Cheese");
+console.log(checkStock(0, "Cheese"));
 // => "Cheese - OUT of stock!"
 
-checkStock(1, "Salsa");
+console.log(checkStock(1, "Salsa"));
 // => "Salsa - running LOW"
